fc060e737e392d8e85730af9eea96ce650341d91
0x67 <gabri.putra@gmail.com>
2023-11-19 02:46:09 +0700

    feat: added support for fastify

--- test/allow-in-submodules.e2e-spec.ts
+++ test/allow-in-submodules.e2e-spec.ts
@@ -1,14 +1,18 @@
 import { INestApplication } from '@nestjs/common';
+import { NestFastifyApplication } from '@nestjs/platform-fastify';
 import * as request from 'supertest';
 import path from 'path';
 import { createTestModule } from './helpers/create-test-module';
 
-describe('Submodule', () => {
+describe('Express - Submodule', () => {
   let appWithGlobal: INestApplication;
   let hasErrorInSubmoduleCreateModuleWithoutGlobal: boolean;
 
   beforeEach(async () => {
-    appWithGlobal = await createTestModule({ isGlobal: true, withSubmodule: true });
+    appWithGlobal = await createTestModule({
+      isGlobal: true,
+      withSubmodule: true,
+    });
 
     try {
       await createTestModule({ isGlobal: false, withSubmodule: true });
@@ -18,7 +22,8 @@ describe('Submodule', () => {
   });
 
   it('Is the module available in submodules', () => {
-    return request.default(appWithGlobal.getHttpServer())
+    return request
+      .default(appWithGlobal.getHttpServer())
       .post('/submodule/single-file')
       .attach('file', path.resolve(__dirname, 'test-files', 'file.txt'))
       .expect(200)
@@ -28,6 +33,40 @@ describe('Submodule', () => {
   it('Is the not module available in submodules', () => {
     return expect(hasErrorInSubmoduleCreateModuleWithoutGlobal).toBe(true);
   });
+});
 
+describe('Fastify - Submodule', () => {
+  let appWithGlobal: NestFastifyApplication;
+  let hasErrorInSubmoduleCreateModuleWithoutGlobal: boolean;
 
+  beforeEach(async () => {
+    appWithGlobal = (await createTestModule({
+      isGlobal: true,
+      withSubmodule: true,
+      fastify: true,
+    })) as NestFastifyApplication;
+
+    try {
+      (await createTestModule({
+        isGlobal: false,
+        withSubmodule: true,
+        fastify: true,
+      })) as NestFastifyApplication;
+    } catch (err) {
+      hasErrorInSubmoduleCreateModuleWithoutGlobal = true;
+    }
+  });
+
+  it('Is the module available in submodules', () => {
+    return request
+      .default(appWithGlobal.getHttpServer())
+      .post('/submodule/single-file')
+      .attach('file', path.resolve(__dirname, 'test-files', 'file.txt'))
+      .expect(200)
+      .expect({ filename: 'file.txt', mimetype: 'text/plain' });
+  });
+
+  it('Is the not module available in submodules', () => {
+    return expect(hasErrorInSubmoduleCreateModuleWithoutGlobal).toBe(true);
+  });
 });
--- test/optional-file-upload.e2e-spec.ts
+++ test/optional-file-upload.e2e-spec.ts
@@ -1,8 +1,9 @@
 import { INestApplication } from '@nestjs/common';
+import { NestFastifyApplication } from '@nestjs/platform-fastify';
 import * as request from 'supertest';
 import { createTestModule } from './helpers/create-test-module';
 
-describe('Optional file uploads', () => {
+describe('Express - Optional file uploads', () => {
   let app: INestApplication;
 
   beforeEach(async () => {
@@ -10,11 +11,28 @@ describe('Optional file uploads', () => {
   });
 
   it('Upload optional file', () => {
-    return request.default(app.getHttpServer())
+    return request
+      .default(app.getHttpServer())
       .post('/optional')
       .expect(200)
       .expect({});
   });
+});
 
+describe('Fastify - Optional file uploads', () => {
+  let app: NestFastifyApplication;
 
+  beforeEach(async () => {
+    app = (await createTestModule({
+      fastify: true,
+    })) as NestFastifyApplication;
+  });
+
+  it('Upload optional file', () => {
+    return request
+      .default(app.getHttpServer())
+      .post('/optional')
+      .expect(200)
+      .expect({});
+  });
 });
